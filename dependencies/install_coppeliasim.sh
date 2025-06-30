49c370ce6012b11d2b578bf397c17906a003dbb2
Juancho <juanjqogm@gmail.com>
2025-06-30 10:11:36 +0100

    [install_coppeliasim.sh] fixed the script file according to the Murilo's suggestions.

--- dependencies/install_coppeliasim.sh
+++ dependencies/install_coppeliasim.sh
@@ -5,21 +5,16 @@
 # https://github.com/CoppeliaRobotics/docker-image-coppeliasim/blob/master/Dockerfile
 # Install dependencies
 apt-get update -q && \
-	export DEBIAN_FRONTEND=noninteractive && \
     apt-get install -y --no-install-recommends \
         vim tar xz-utils \
         libx11-6 libxcb1 libxau6 libgl1-mesa-dev \
         xvfb dbus-x11 x11-utils libxkbcommon-x11-0 \
         libavcodec-dev libavformat-dev libswscale-dev \
         python3 python3-pip python3-venv libraw1394-11 libmpfr6 \
-        libusb-1.0-0 \
-        && \
-    apt-get autoclean -y && apt-get autoremove -y && apt-get clean && \
-    rm -rf /var/lib/apt/lists/*
+        libusb-1.0-0 
 
 python3 -m venv /opt/venv
-ENV PATH="/opt/venv/bin:$PATH"
-pip3 install pyzmq cbor2    
+pip3 install pyzmq cbor2 --break-system-packages
 
 # Download CoppeliaSim
 mkdir -p ~/utils
