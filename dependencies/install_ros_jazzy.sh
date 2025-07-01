365e4c18615acb444514751af3add7add858a8f9
Juancho <juanjqogm@gmail.com>
2025-07-01 16:55:23 +0100

    Added the apt-get clean and rm -rf /var/lib/apt/lists/ to reduce the cache size

--- dependencies/install_ros_jazzy.sh
+++ dependencies/install_ros_jazzy.sh
@@ -5,7 +5,9 @@ add-apt-repository universe
 curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | tee /etc/apt/sources.list.d/ros2.list > /dev/null
 apt update && apt upgrade -y
-apt-get update && apt-get install -y ros-jazzy-desktop ros-dev-tools
+apt-get update && apt-get install -y ros-jazzy-desktop ros-dev-tools 
+apt-get clean 
+rm -rf /var/lib/apt/lists/*
 
 mkdir -p ~/ros2_ws/src
 
